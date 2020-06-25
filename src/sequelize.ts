a3075fb46f952d882f034943912fd43a48ef3f65
TY <skssmsdlxl@snu.ac.kr>
2020-06-25 11:31:38 +0900

    final version

---
+++ src/sequelize.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from 'sequelize-typescript';
+
+export const sequelize = new Sequelize({
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: 'taeyun',
+  host: process.env.DB_HOST_DEV,
+  dialect: 'mariadb',
+  timezone: process.env.DB_TIMEZONE,
+  dialectOptions: {
+    charset: 'utf8mb4',
+    dateStrings: true,
+    typeCast: true,
+    timezone: process.env.DB_TIMEZONE,
+  },
+  models: [`${__dirname}/models`],
+  define: {
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+    timestamps: true,
+  },
+});
